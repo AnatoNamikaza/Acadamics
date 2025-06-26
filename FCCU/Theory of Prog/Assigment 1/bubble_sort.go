e76a3774f1294665a38022e3cb9b7268546a46ca
Anato Namikaza <anato.namkikaza.01@gmail.com>
2025-06-27 03:54:33 +0500

    Create bubble_sort.go

---
+++ FCCU/Theory of Prog/Assigment 1/bubble_sort.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func bubbleSort(arr []int) {
+    n := len(arr)
+    for i := 0; i < n-1; i++ {
+        for j := 0; j < n-i-1; j++ {
+            if arr[j] > arr[j+1] {
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            }
+        }
+    }
+}
+
+func main() {
+    arr := []int{5, 3, 8, 4, 2}
+    bubbleSort(arr)
+    fmt.Println(arr)
+}
