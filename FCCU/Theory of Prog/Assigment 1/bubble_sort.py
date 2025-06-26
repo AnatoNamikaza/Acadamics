94c8a909afdebc090a0d0cefeb1d2f12ba39da26
Anato Namikaza <anato.namkikaza.01@gmail.com>
2025-06-27 03:55:04 +0500

    Create bubble_sort.py

---
+++ FCCU/Theory of Prog/Assigment 1/bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+arr = [5, 3, 8, 4, 2]
+print(bubble_sort(arr))
